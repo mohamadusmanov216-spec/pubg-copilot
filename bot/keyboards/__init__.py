2ceb370c70cb65bbdf86c3e8890a0b468e67b897
mohamadusmanov216-spec <mohamadusmanov216@gmail.com>
2026-01-04 18:02:56 +0300

    Update __init__.py

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,2 +1,7 @@
 from bot.keyboards.main_menu import main_menu_keyboard
 from bot.keyboards.matchmaking_menu import matchmaking_keyboard
+
+__all__ = [
+    "main_menu_keyboard",
+    "matchmaking_keyboard",
+]
