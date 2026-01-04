94c84f2759aa471e7df2d5b9001b600310dda66a
mohamadusmanov216-spec <mohamadusmanov216@gmail.com>
2026-01-04 12:32:41 +0300

    Update main_menu.py

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -1,1 +1,19 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+def main_menu_keyboard():
+    keyboard = [
+        [KeyboardButton(text="🎮 Советы по PUBG")],
+        [KeyboardButton(text="🔫 Подбор оружия")],
+        [KeyboardButton(text="🗺 Тактики по картам")],
+        [KeyboardButton(text="🤖 ИИ-чат")],
+        [KeyboardButton(text="👥 Поиск тиммейтов")],
+        [KeyboardButton(text="🎭 Генератор никнеймов")],
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True,
+        input_field_placeholder="Выбери действие…"
+    )
 
