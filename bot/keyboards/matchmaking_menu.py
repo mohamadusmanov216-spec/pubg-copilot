7b7a7b240d49411078aae22e76a908513accaddc
mohamadusmanov216-spec <mohamadusmanov216@gmail.com>
2026-01-04 13:57:33 +0300

    Update matchmaking_menu.py

--- bot/keyboards/matchmaking_menu.py
+++ bot/keyboards/matchmaking_menu.py
@@ -1,1 +1,13 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
+def matchmaking_keyboard():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📝 Заполнить профиль")],
+            [KeyboardButton(text="🔍 Начать поиск")],
+            [KeyboardButton(text="⛔ Остановить поиск")],
+            [KeyboardButton(text="👤 Мой профиль")],
+        ],
+        resize_keyboard=True
+    )
